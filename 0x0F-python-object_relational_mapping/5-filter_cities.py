f8018dfacba6d855d161dab7b757f9a96c7772e2
ophiliatex <ophiliatex@gmail.com>
2024-02-08 04:59:22 -0800

    fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,37 @@
-#!/usr/bin/python3 
- """  lists all states from the database hbtn_0e_0_usa """ 
- import MySQLdb 
- import sys 
-  
-  
- if __name__ == "__main__": 
-     db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
-                          passwd=sys.argv[2], db=sys.argv[3], port=3306) 
-     cur = db.cursor() 
-     cur.execute("""SELECT cities.name FROM 
-                 cities INNER JOIN states ON states.id=cities.state_id 
-                 WHERE states.name=%s""", (sys.argv[4],)) 
-     rows = cur.fetchall() 
-     tmp = list(row[0] for row in rows) 
-     print(*tmp, sep=", ") 
-     cur.close() 
-     db.close()
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """"
+    Access to the database and get the cities
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("""
+        SELECT
+            cities.name
+        FROM
+            cities
+        JOIN
+            states
+        ON
+            cities.states_id = state.id
+        WHERE
+            states.name LIKE BINARY %(state_name)s
+        ORDER BY
+            cities.id ASC
+        """, {
+            'state_name': sys.argv[4]
+        })
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
