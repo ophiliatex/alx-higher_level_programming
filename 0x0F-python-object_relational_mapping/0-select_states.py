cbcaa7278016633fbe6620c18f375fe0b40f524a
ophiliatex <ophiliatex@gmail.com>
2024-02-08 05:48:31 -0800

    ck

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""
-lists all states from the database hbtn_0e_0_usa
+"""lists all states from the.
+database hbtn_0e_0_usa.
 """
 
 import MySQLdb
@@ -8,8 +8,7 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    """
-    Access to the database and get the states
+    """Access to the database and get the states
     from the database.
     """
     db = MySQLdb.connect(host="localhost", user=argv[1],
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
-"""
-lists all states from the database hbtn_0e_0_usa
+"""lists all states from the.
+database hbtn_0e_0_usa.
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    
     """
     Access database and get the states
-    from the database
+    from the database.
+
     """
 
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
