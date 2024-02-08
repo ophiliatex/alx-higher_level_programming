3577378a9dcc211dfd7ae94e47c3e1c18ff33570
ophiliatex <ophiliatex@gmail.com>
2024-02-08 15:05:19 +0000

    chfx

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,38 @@
 #!/usr/bin/python3
 
 """
-lists all states from the database hbtn_0e_0_usa
+Script: list_states.py
+
+Description:
+This script lists all states from the database 'hbtn_0e_6_usa' with the name
+passed as an argument. It connects to the MySQL database using the provided
+credentials and retrieves the states whose names start with 'N', displaying
+the results in the order of state IDs.
+
+Usage:
+python3 list_states.py <MySQL_username> <MySQL_password> <database_name>
+
+Example:
+python3 list_states.py myusername mypassword hbtn_0e_6_usa
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     """
-    Access to the database and get the states
-    from the database.
+    Main function to execute the script.
+
+    - Connects to the MySQL database using provided credentials.
+    - Executes a SQL query to retrieve states with names starting with 'N'.
+    - Displays the results in the order of state IDs.
+
+    Arguments:
+    sys.argv[1]: MySQL username
+    sys.argv[2]: MySQL password
+    sys.argv[3]: Database name
+
+    Usage Example:
+    python3 list_states.py myusername mypassword hbtn_0e_6_usa
     """
 
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3 
 """
-Start link class to table in database
+Script: list_states.py
+
+Description:
+This script lists all states from the database 'hbtn_0e_6_usa' with the name
+passed as an argument. It connects to the MySQL database using the provided
+credentials and retrieves the states whose names start with 'N', displaying
+the results in the order of state IDs.
+
+Usage:
+python3 list_states.py <MySQL_username> <MySQL_password> <database_name>
+
+Example:
+python3 list_states.py myusername mypassword hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -8,8 +20,19 @@ from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
     """
-    Access database from listed
-    databases
+    Main function to execute the script.
+
+    - Connects to the MySQL database using provided credentials.
+    - Executes a SQL query to retrieve states with names starting with 'N'.
+    - Displays the results in the order of state IDs.
+
+    Arguments:
+    sys.argv[1]: MySQL username
+    sys.argv[2]: MySQL password
+    sys.argv[3]: Database name
+
+    Usage Example:
+    python3 list_states.py myusername mypassword hbtn_0e_6_usa
     """
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
