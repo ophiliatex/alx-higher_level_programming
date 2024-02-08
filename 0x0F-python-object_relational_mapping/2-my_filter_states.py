948de4badfa4e93f8db1b1ba0560516dd5577941
ophiliatex <ophiliatex@gmail.com>
2024-02-08 15:44:27 +0000

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,48 +4,57 @@
 Script: list_states_dynamic.py
 
 Description:
-This script lists all states from the database 'hbtn_0e_6_usa' with the name
-passed as an argument. It connects to the MySQL database using the provided
-credentials and retrieves the states whose names start with 'N', displaying
-the results in the order of state IDs.
+This script connects to a MySQL database and retrieves states based on a provided name pattern. It takes MySQL credentials, database name, and name pattern as command-line arguments.
 
 Usage:
-python3 list_states_dynamic.py <MySQL_username> <MySQL_password> <database_name>
+python3 list_states_dynamic.py <MySQL_username> <MySQL_password> <database_name> <name_pattern>
 
 Example:
-python3 list_states_dynamic.py myusername mypassword hbtn_0e_6_usa
-""" 
- 
+python3 list_states_dynamic.py myusername mypassword hbtn_0e_6_usa 'N%'
+"""
+
+# Import necessary modules
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
+
     """
     Main function to execute the script.
 
     - Connects to the MySQL database using provided credentials.
-    - Executes a SQL query to retrieve states with names starting with 'N'.
-    - Displays the results in the order of state IDs.
+    - Executes a dynamic SQL query to retrieve states based on the name pattern.
+    - Displays the results ordered by state IDs.
 
     Arguments:
     sys.argv[1]: MySQL username
     sys.argv[2]: MySQL password
     sys.argv[3]: Database name
+    sys.argv[4]: Name pattern for states (e.g., 'N%')
 
     Usage Example:
-    python3 list_states_dynamic.py myusername mypassword hbtn_0e_6_usa
+    python3 list_states_dynamic.py myusername mypassword hbtn_0e_6_usa 'N%'
     """
 
+    # Connect to the MySQL database
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
+    # Create a cursor object to execute SQL queries
     cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-            WHERE name LIKE BINARY '{}' \
-            ORDER BY states.id ASC".format(sys.argv[4]))
-     rows = cur.fetchall()
 
-     for row in rows:
-         print(row)
-     db.close()
+    # Execute dynamic SQL query to retrieve states based on name pattern
+    cur.execute("SELECT * FROM states \
+                WHERE name LIKE BINARY '{}' \
+                ORDER BY states.id ASC".format(sys.argv[4]))
+
+    # Fetch all rows from the result set
+    rows = cur.fetchall()
+
+    # Display the retrieved rows
+    for row in rows:
+        print(row)
+
+    # Close the database connection
+    db.close()
+
