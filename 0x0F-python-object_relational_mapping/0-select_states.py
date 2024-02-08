785940aee79097483ac02f3087a42fbbb61842f9
ophiliatex <ophiliatex@gmail.com>
2024-02-08 02:30:57 -0800

    ck

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3 
- """  
- lists all states from the database hbtn_0e_0_usa
- """
+"""  
+lists all states from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
-from sys import argv 
+from sys import argv
 
- if __name__ == '__main__':
-     """
-     Access the database and get the states
-     from the database.
-     """
+if __name__ == '__main__':
 
-     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-             db=argv[3])
-     
-     cur = db.cursor() 
-     cur.execute("SELECT * FROM states") 
-     rows = cur.fetchall()
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor() 
+    cur.execute("SELECT * FROM states") 
+    rows = cur.fetchall()
 
-    for row in rows: 
-         print(row)
+    for row in rows:
+        print(row)
